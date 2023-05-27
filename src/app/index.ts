76f0dddce6195bbaef4991887647d4358175f75f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 19:03:52 +0100

    feat: add body-parser to parse request data, configure passport-jwt with app, add type for nigeria-geo, configure error handler with app'

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,17 +1,31 @@
 import express, { Express, Request, Response } from 'express'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import nigeriaLocations from 'nigeria-geo'
+import nigeriaLocations, { NigeriaLocation } from 'nigeria-geo'
+import passport from 'passport'
+import bodyParser from 'body-parser'
+
 import authRouter from './routes/auth.route'
+import passportMiddleware from './middlewares/passport.middleware'
+import errorHandlerMiddleware from './middlewares/errorHandler.middleware'
+
 require('dotenv').config()
 
 const app: Express = express()
 
-app.get('/', (req: Request, res: Response) => {
-  const nl = nigeriaLocations.all()
-  res.json(nl)
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+passportMiddleware(passport)
+
+app.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req: Request, res: Response) => {
+    const nl: NigeriaLocation = nigeriaLocations.all()
+    res.json(nl)
+  }
+)
 
 app.use('/auth', authRouter)
+app.use(errorHandlerMiddleware)
 
 export default app
