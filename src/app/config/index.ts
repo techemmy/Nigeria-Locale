54100432e40b1d0ea89cf0fa8a8fffe8fc7fd72b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 20:34:15 +0100

    refactor: change config export method

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,8 +2,4 @@ const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI ?? ''
 const JWT_Secret = process.env.JWT_SECRET
 
-export default {
-  PORT,
-  MONGO_URI,
-  JWT_Secret
-}
+export { PORT, MONGO_URI, JWT_Secret }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import app from './app'
-import CONFIG from './app/config'
+import { MONGO_URI, PORT } from './app/config'
 import connectMongoDB from './app/database'
 
-connectMongoDB(CONFIG.MONGO_URI)
+connectMongoDB(MONGO_URI)
 
-app.listen(CONFIG.PORT, () => {
-  console.log(
-    `⚡️[server]: Server is running at http://localhost:${CONFIG.PORT}`
-  )
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
