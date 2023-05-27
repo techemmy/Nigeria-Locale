2bd81520c56ba5e99d698bcc38a6b29de72d74b9
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 21:42:09 +0100

    feat: implement login functionality

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -3,6 +3,7 @@ import User, { IUser } from '../models/user.model'
 import { HydratedDocument } from 'mongoose'
 import jwt from 'jsonwebtoken'
 import config from '../config'
+import userModel from '../models/user.model'
 
 export async function signup(req: Request, res: Response, next: NextFunction) {
   try {
@@ -29,6 +30,31 @@ export async function signup(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export function login(req: Request, res: Response) {
-  return res.json({ message: 'login succesful' })
+export async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { username, password } = req.body
+    const user = await userModel.findOne({ username })
+    const isUserPasswordCorrect = await user?.verifyPassword(password)
+
+    if (!isUserPasswordCorrect) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid credentials',
+        data: {},
+        error_code: 401
+      })
+    }
+
+    const payload = { id: user?.id, username: user?.username }
+    const loginToken = jwt.sign(payload, config.JWT_Secret, {
+      expiresIn: '7d'
+    })
+    return res.status(200).json({
+      success: true,
+      message: 'login succesful',
+      data: { loginToken }
+    })
+  } catch (error) {
+    return next(error)
+  }
 }
