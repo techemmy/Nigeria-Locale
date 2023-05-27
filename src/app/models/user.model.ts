ff200c25d7509bab0fdcb00262ce66b84ddb02e9
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 19:10:26 +0100

    feat: create User model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose'
+import bcrypt from 'bcrypt'
+import { generateAPIKey, verifyAPIKey } from '../utils/APIKey.utils'
+
+export interface IUser {
+  username: string
+  password: string
+  APIkey: string
+  getAPIKey(): string
+  verifyAPIKey(APIKey: string): string
+}
+
+const userSchema = new Schema<IUser>({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  APIkey: String
+})
+
+userSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 15)
+})
+
+userSchema.methods.verifyPassword = async function (password: string) {
+  const isPassword = await bcrypt.compare(password, this.password)
+  return isPassword
+}
+
+userSchema.methods.getAPIKey = async function () {
+  const key = await generateAPIKey(this.username)
+  this.APIKey = key
+  return key
+}
+
+userSchema.methods.verifyAPIKey = async function (APIKey: string) {
+  const isUserAPIKey = verifyAPIKey(APIKey, this.APIKey)
+  return isUserAPIKey
+}
+
+export default model<IUser>('User', userSchema)
