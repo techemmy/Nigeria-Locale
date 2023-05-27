6edb18d2c010c55da420fedcf9b344b4519198d1
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 22:34:26 +0100

    feat: update type declarations for generateJWTLoginToken parameters

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken'
 import config from './config'
 
-export function generateJWTLoginToken(userId: string, username: string) {
+export function generateJWTLoginToken(
+  userId: string | undefined,
+  username: string | undefined
+) {
   if (!userId || !username) return
 
   return jwt.sign({ id: userId, username }, config.JWT_Secret, {
