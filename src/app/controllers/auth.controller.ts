eea87dfed18c9163bde23ac4e42c8702cb21ad4c
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 19:16:03 +0100

    feat: implemented authentication for user signup

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -1,9 +1,26 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import User, { IUser } from '../models/user.model'
+import { HydratedDocument } from 'mongoose'
 
-export function signup(req: Request, res: Response): Response {
-  return res.json({ message: 'signup successful' })
+export async function signup(req: Request, res: Response, next: NextFunction) {
+  try {
+    console.log(req.body)
+    const { username, password } = req.body
+
+    const user: HydratedDocument<IUser> = new User({ username, password })
+    const userAPIKey = await user.getAPIKey()
+    await user.save()
+
+    return res.status(201).json({
+      success: true,
+      message: 'signup successful',
+      data: { userAPIKey }
+    })
+  } catch (error) {
+    return next(error)
+  }
 }
 
-export function login(req: Request, res: Response): Response {
+export function login(req: Request, res: Response) {
   return res.json({ message: 'login succesful' })
 }
