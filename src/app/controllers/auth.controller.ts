552cd6d6c471e4356b10da74f0dc642ea8c4f376
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-05-27 19:57:50 +0100

    style: remove comment

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -4,7 +4,6 @@ import { HydratedDocument } from 'mongoose'
 
 export async function signup(req: Request, res: Response, next: NextFunction) {
   try {
-    console.log(req.body)
     const { username, password } = req.body
 
     const user: HydratedDocument<IUser> = new User({ username, password })
