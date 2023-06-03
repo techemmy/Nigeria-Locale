313eeb9aa163f2d365451a260bc82798a3a2a0b6
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-04 00:51:03 +0100

    feat(app): configure redis to start when server starts and export it for use throughout the app

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
+import redis from 'redis'
 import app from './app'
 import config from './app/config'
-import connectMongoDB from './app/database'
+import { connectMongoDB, connectRedis } from './app/database'
 
+let redisClient: redis.RedisClientType
 connectMongoDB(config.MONGO_URI)
+
+void (async () => {
+  redisClient = await connectRedis()
+})()
 
 app.listen(config.PORT, () => {
   console.log(
     `⚡️[server]: Server is running at http://localhost:${config.PORT}`
   )
 })
+
+export { redisClient }
