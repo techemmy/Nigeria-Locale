8285ac942c1b7abc2cc7e247a2c8766222b41585
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-06 22:04:54 +0100

    fix(cache): stop adding of empty keys to the redis cache db

--- src/app/middlewares/cacheData.middleware.ts
+++ src/app/middlewares/cacheData.middleware.ts
@@ -7,10 +7,16 @@ export default async function (
   next: NextFunction
 ) {
   const { category, query } = req.query
-  const cachedData: string =
-    (await redisClient.get(`${category}-${query}`)) ?? '{}'
-  const parsedCache = JSON.parse(cachedData)
+  const cachedData: string | null = await redisClient.get(
+    `${category}-${query}`
+  )
 
+  if (!cachedData) {
+    return next()
+  }
+
+  const parsedCache = JSON.parse(cachedData)
+  console.log(parsedCache, await redisClient.get(`${category}-${query}`))
   if (category === parsedCache?.category && query === parsedCache?.query) {
     return res.status(200).json({
       success: true,
