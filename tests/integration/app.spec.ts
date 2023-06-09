8d4351a2a4124d74b46c19056c6b20496ebcb70b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-09 20:48:32 +0100

    test: add test to check for the right content-type and ensure the request rate limit is set properly

--- tests/integration/app.spec.ts
+++ tests/integration/app.spec.ts
@@ -1,7 +1,19 @@
 import app from '../../src/app'
 import request from 'supertest'
 
-test('app works at GET /', async () => {
-  const response = await request(app).get('/')
-  expect(response.status).toBe(200)
+describe('Make sure server starts and with the right configuration', () => {
+  test('app server starts at GET /', async () => {
+    const response = await request(app).get('/')
+    expect(response.status).toBe(200)
+  })
+
+  test('app server returns the right content type', async () => {
+    const response = await request(app).get('/')
+    expect(response.headers['content-type']).toContain('application/json')
+  })
+
+  test('app server has default rate-limit set', async () => {
+    const response = await request(app).get('/')
+    expect(parseInt(response.headers['ratelimit-limit'])).toBe(100)
+  })
 })
