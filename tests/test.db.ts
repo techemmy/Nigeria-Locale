ca0eca91563981514243a69ec42fa4e6d022848f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-10 23:10:19 +0100

    style: add typescript return types for the functions

--- tests/test.db.ts
+++ tests/test.db.ts
@@ -3,14 +3,14 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 
 let testDB
 
-export async function connectDB() {
+export async function connectDB(): Promise<void> {
   testDB = await MongoMemoryServer.create()
   const uri = testDB.getUri()
 
   await mongoose.connect(uri)
 }
 
-export async function cleanUp() {
+export async function cleanUp(): Promise<void> {
   if (testDB) {
     const collections = await mongoose.connection.db.collections()
     for (const collection of collections) {
@@ -19,7 +19,7 @@ export async function cleanUp() {
   }
 }
 
-export async function disconnectDB() {
+export async function disconnectDB(): Promise<void> {
   if (testDB) {
     await mongoose.connection.dropDatabase()
     await mongoose.connection.close()
