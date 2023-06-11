3cf1e4fd1f67537ffd8dc7878f61778b27dbf106
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-12 00:34:32 +0100

    test: add test to ensure required data is gotten from the response json object

--- tests/integration/auth/login.spec.ts
+++ tests/integration/auth/login.spec.ts
@@ -16,11 +16,21 @@ afterAll(async () => {
 })
 
 describe('POST /auth/login', () => {
+  const { username, password } = users[0]
+
   test('should log user in successfully', async () => {
-    const { username, password } = users[0]
     const response = await request(app)
       .post('/auth/login')
       .send({ username, password })
     expect(response.status).toBe(200)
+  })
+
+  test('should log user in with a valid json response', async () => {
+    const response = await request(app)
+      .post('/auth/login')
+      .send({ username, password })
+    expect(response.body.success).toBeTruthy()
+    expect(response.body.data.loginToken).toBeDefined()
+    expect(response.headers['content-type']).toContain('application/json')
   })
 })
