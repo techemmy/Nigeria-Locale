a560ebb4e38b85dc205e7cec908d8565e7b4247a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-13 23:41:55 +0100

    feat(route): add validator middlwares to the authentication routes

--- src/app/routes/auth.route.ts
+++ src/app/routes/auth.route.ts
@@ -1,9 +1,14 @@
 import express, { Router } from 'express'
 import * as authController from '../controllers/auth.controller'
+import validatorsMiddleware from '../middlewares/validators.middleware'
 
 const authRouter: Router = express.Router()
 
-authRouter.post('/signup', authController.signup)
-authRouter.post('/login', authController.login)
+authRouter.post(
+  '/signup',
+  validatorsMiddleware('signup'),
+  authController.signup
+)
+authRouter.post('/login', validatorsMiddleware('login'), authController.login)
 
 export default authRouter
