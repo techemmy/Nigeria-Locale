682733ac0d616a5ff824d43b343c168410fde6d9
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-14 20:26:27 +0100

    test: add new test cases perf: refactor potential multiple request call into the beforeAll pre-test runners

--- spec/integration/routes/location/getNewAPIKey.spec.ts
+++ spec/integration/routes/location/getNewAPIKey.spec.ts
@@ -5,8 +5,13 @@ import users from '../../../fixtures/users.json'
 import jwt from 'jsonwebtoken'
 import config from '../../../../src/app/config'
 
+const PATH = '/api/v1/get-new-key'
+let loginToken: string
+
 beforeAll(async () => {
   await connectDB()
+  loginToken = (await request(app).post('/auth/signup').send(users[0])).body
+    .data.loginToken
 })
 
 afterAll(async () => {
@@ -14,18 +19,26 @@ afterAll(async () => {
   await disconnectDB()
 })
 
-const PATH = '/api/v1/get-new-key'
-
 describe(`GET ${PATH}`, () => {
   test('should generate a new API key for user successfully', async () => {
-    const loginToken = (await request(app).post('/auth/signup').send(users[0]))
-      .body.data.loginToken
-
     const response = await request(app)
       .get(PATH)
       .set('Authorization', `Bearer ${loginToken}`)
 
     expect(response.status).toBe(201)
+  })
+
+  test('should generate a new API key with a valid json response', async () => {
+    const response = await request(app)
+      .get(PATH)
+      .set('Authorization', `Bearer ${loginToken}`)
+
+    expect(response.status).toBe(201)
+    expect(response.body).toBeDefined()
+    expect(response.body.success).toBeTruthy()
+    expect(response.body.message).toBeDefined()
+    expect(response.body.data.APIKey).toBeDefined()
+    expect(response.headers['content-type']).toContain('application/json')
   })
 
   test('should fail to generate API key if no auth header is found', async () => {
