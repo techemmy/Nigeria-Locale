5df3299e82f6e103da166faf05e35aee8d47bee0
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-06-23 06:38:24 +0100

    feat: configure REDIS_URL

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -3,9 +3,14 @@ require('dotenv').config()
 const PORT: string = process.env.PORT ?? '5000'
 const MONGO_URI: string = process.env.MONGO_URI ?? ''
 const JWT_Secret: string = process.env.JWT_SECRET ?? ''
+const REDIS_URL =
+  process.env.NODE_ENV === 'development'
+    ? process.env.REDIS_DEV_URL ?? ''
+    : process.env.REDIS_PRDD_URL ?? ''
 
 export default {
   PORT,
   MONGO_URI,
-  JWT_Secret
+  JWT_Secret,
+  REDIS_URL
 }
